761df772acd6bafcddd218ec386d19bebbf7ac47
zampou-spec <zampou.elec@gmail.com>
2021-09-08 13:42:34 +0000

    fix empty result

--- server.js
+++ server.js
@@ -5,43 +5,49 @@ const impotSalaire = require('./functions/impotSalaire');
 
 const app = express();
 
-
 const PORT = 8000
 
 // log request
 app.use(morgan('tiny'));
-
-
 
 const MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 
 // parse request 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 app.get('/:id', (req, res) => {
-    
-     MongoClient.connect(url, { useUnifiedTopology: true}, function(err, db) {
+     MongoClient.connect(url, {useUnifiedTopology: true}, function(err, db) {
         if (err) throw err;
         var dbo = db.db("Microservice_is");
         const id = parseInt(req.params.id);
         dbo.collection("assures").findOne({
             id: id
-        }, 
+        },
         function(err, result) {
             if (err) throw err;
-            let assure = result;
-            let {nom, montant_brut, id} = assure;
-            let montantImpot = impotSalaire(montant_brut);
-            res.json({
-                'id': id,
-                'nom': nom,
-                'montant brut': montant_brut,
-                'montant impot': montantImpot
-            })
+
+            if (result) {
+                let {nom, montant_brut, id} = result;
+                let montant_impot = impotSalaire(montant_brut);
+                res.json({
+                    'status': 200,
+                    'result': {
+                        'id': id,
+                        'nom': nom,
+                        'montant brut': montant_brut,
+                        'montant impot': montant_impot
+                    }
+                })
+            } else {
+                res.json({
+                    'status': 404,
+                    'result': null
+                })
+            }
             
             db.close();
         });
